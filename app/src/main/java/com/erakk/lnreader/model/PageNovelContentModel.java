b23c39dbd0e183ff1fcdd8bef4c1c94afa738a2f
jeremy.masclaux <info@masclaux.net>
2015-03-25 15:22:17 +0100

    First commit => try to implement a reading mode like => epub pdf  ( flow mode, page + show  picture on full screen ).

---
+++ app/src/main/java/com/erakk/lnreader/model/PageNovelContentModel.java
@@ -0,0 +1,70 @@
+package com.erakk.lnreader.model;
+
+import java.util.ArrayList;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * Created by jeremy.masclaux on 25/03/2015.
+ */
+public class PageNovelContentModel extends NovelContentModel
+{
+    //max character in one page
+    public static int MAX_CHARACTER_PAGE = 1400;
+
+    //max block in one page
+    public static int MAX_BLOC_PAGE = 35;
+
+    //current page ( we generate virtual page from the text of virtual page)
+    private int currentPage = 0;
+
+
+    private ArrayList<String> pages = new ArrayList<>();
+
+    //same as base class + current page
+    public String getPage()
+    {
+        return super.getPage() + " page " + currentPage;
+    }
+
+    public void setContent(String content)
+    {
+        //Check number of page
+        Pattern p = Pattern.compile("<p\\b[^>]*>(.*?)</p>"); // get all para
+        Matcher m = p.matcher(content);
+
+        int tempPara = 0;
+        String tempParaText = "";
+
+        while(m.find())
+        {
+            if ( m.group().length() != 0 )
+            {
+                tempPara++;
+                tempParaText += m.group();
+            }
+
+            if( tempParaText.length() >= MAX_CHARACTER_PAGE || tempPara >= MAX_BLOC_PAGE )
+            {
+                pages.add(tempParaText);//new page
+                tempPara     = 0;
+                tempParaText ="";
+            }
+        }
+
+        //Last Page
+        if( tempParaText.length() > 0 )
+        {
+            pages.add(tempParaText);
+        }
+
+        super.setContent( content );
+    }
+
+    public String getContent()
+    {
+        return super.getContent();
+    }
+}
